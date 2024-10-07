a6843a37a541f91d4572929dfb272516eb9ff0b8
FAYSi223 <134807221+FAYSi223@users.noreply.github.com>
2024-10-07 21:45:33 +0200

    Update applyRoleToMember.js
    Maybe works?

--- src/routes/servers/roles/applyRoleToMember.js
+++ src/routes/servers/roles/applyRoleToMember.js
@@ -10,13 +10,11 @@ module.exports = async (req, res, next) => {
     const { server_id, member_id, role_id } = req.params;
 
     const user = await Users.findOne({ id: member_id });
-
     if (!user) {
       return res.status(404).json({ message: "User does not exist." });
     }
 
     const role = await ServerRoles.findOne({ id: role_id, server_id: server_id }).select("bot order default");
-
     if (!role || role.default === true) {
       return res.status(404).json({ message: "Role does not exist." });
     }
@@ -26,7 +24,6 @@ module.exports = async (req, res, next) => {
     }
 
     const serverMember = await ServerMembers.findOne({ server_id: server_id, member: user._id });
-
     if (!serverMember) {
       return res.status(404).json({ message: "Member does not exist." });
     }
@@ -36,21 +33,17 @@ module.exports = async (req, res, next) => {
       return res.status(403).json({ message: "Your role priority is too low to perform this action." });
     }
 
-
     const updateResult = await ServerMembers.updateOne(
       { _id: serverMember._id },
       { $addToSet: { roles: role_id } }
     );
 
-   
     if (updateResult.nModified === 0) {
       return res.status(200).json({ message: "Role already assigned to the member." });
     }
 
-   
     redis.remServerMember(member_id, server_id);
 
- 
     const io = req.io;
     io.in("server:" + server_id).emit(SERVER_ROLE_ADDED_TO_MEMBER, {
       role_id: role_id,
